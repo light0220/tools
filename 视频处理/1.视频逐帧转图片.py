b8816b497ed25865a79194122b7446e63c5361fb
孙振光 <light22@126.com>
2022-10-05 01:19:21 +0800

    modified:   "\350\247\206\351\242\221\345\244\204\347\220\206/1.\350\247\206\351\242\221\351\200\220\345\270\247\350\275\254\345\233\276\347\211\207.py"

--- 视频处理/1.视频逐帧转图片.py
+++ 视频处理/1.视频逐帧转图片.py
@@ -1,18 +1,17 @@
 # 使用这篇代码将视频逐帧分割
 import cv2
 import os
-def save_img():
-    video_path = './练习文件/视频处理/视频源/' # 保存视频的路径
-    video_name = os.listdir(video_path)[0] # 视频的名字
-    file_name = video_name.split('.')[0]
-    os.makedirs('D:/Desktop/pic/', exist_ok=True)
-    vc = cv2.VideoCapture(video_path+'/'+video_name)
-    c=0
-    rval=vc.isOpened()
+
+
+def save_img(video_path, pic_dir):
+    os.makedirs(pic_dir, exist_ok=True)
+    vc = cv2.VideoCapture(video_path)
+    c = 0
+    rval = vc.isOpened()
     while rval:
-        c = c + 1
+        c += 1
         rval, frame = vc.read()
-        pic_path = 'D:/Desktop/pic/'
+        pic_path = pic_dir
         if rval:
             cv2.imwrite(pic_path + str(c) + '.jpg', frame)
             cv2.waitKey(1)
@@ -20,7 +19,9 @@ def save_img():
             break
     vc.release()
     print('save_success')
-    print(file_name)
 
-    
-save_img()
+
+if __name__ == '__main__':
+    video_path = "D:/Desktop/11.mp4"  # 源视频的路径
+    pic_dir = "D:/Desktop/pic/"  # 图片保存目录，一定要以 '/' 结尾
+    save_img(video_path, pic_dir)
